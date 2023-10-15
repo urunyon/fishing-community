d13e5e085d49978856bf0529ed10fdc64083c897
urunyon <141293154+urunyon@users.noreply.github.com>
2023-10-15 10:06:30 +0000

    [Add]before.deploy

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -3,14 +3,20 @@ class Public::CommentsController < ApplicationController
   
   def create
     post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_image_id = post_image.id
+    comment = current_user.comments.new(comment_params)
+    comment.post_id = post.id
     comment.save
-    redirect_to post_image_path(post_image)
+    redirect_to post_path(post)
   end
   
+  #def destroy
+    #Comment.find(params[:id]).destroy
+    #redirect_to post_path(params[:post_id])
+  #end
   def destroy
-    Comment.find(params[:id]).destroy
+    @post = Post.find(params[:id])
+    Comment.find(params[:id])
+    comment.destroy
     redirect_to post_path(params[:post_id])
   end
   
