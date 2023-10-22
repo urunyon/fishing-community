de6fd17f01ab4e11bb57b3ba05b9816c025cf050
urunyon <141293154+urunyon@users.noreply.github.com>
2023-10-22 08:19:37 +0000

    [Add]layout

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,10 +2,11 @@ class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
 
  	def search
-   @model = params[:model]
-   @content = params[:content]
-   @method = params[:method]
-   @records = Post.search_for(@content, @method)
+    @model = params[:model]
+    @content = params[:content]
+    @method = params[:method]
+    @records = Post.search_for(@content, @method)
+    @genres = Genre.all
   end
   
 end
