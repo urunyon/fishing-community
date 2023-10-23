20d60420c7154f74648b8237e62ad6461ec551d9
urunyon <141293154+urunyon@users.noreply.github.com>
2023-10-23 10:13:46 +0000

    [Add]users.index

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,10 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!
+  
+  def index
+    @users = User.page(params[:page]).per(10)
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
