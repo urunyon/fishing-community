455cc801adf134a68557adac5210401db6e64a73
urunyon <141293154+urunyon@users.noreply.github.com>
2023-10-23 07:15:32 +0000

    [Add]favorite.Ajax

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,18 +1,18 @@
 class Public::FavoritesController < ApplicationController
   before_action :authenticate_user!
 
+ #非同期通信の為「いいね」のリンク先は必要ない
+
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = @post.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to post_path(post)
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = @post.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    redirect_to post_path(post)
   end
 
 
