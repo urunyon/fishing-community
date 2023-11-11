c69c7779a3013995fe6cc8a93cb7e959c412d695
urunyon <141293154+urunyon@users.noreply.github.com>
2023-11-11 05:04:30 +0000

    [Add]S3 ActiveStorage

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,13 +13,7 @@ class Post < ApplicationRecord
   scope :latest, -> { order(created_at: :desc) }  #desc = 降順
   scope :old, -> { order(created_at: :asc) }  #asc = 昇順
 
-  def get_post_image(width, height)
-    unless post_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.png')
-      post_image.attach(io: File.open(file_path), filename: 'no_image.png', content_type: 'image/png')
-    end
-    post_image.variant(resize_to_limit: [width, height]).processed
-  end
+
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
