b780361a7d56459a16426fc0672a724f9ff7a4c8
urunyon <141293154+urunyon@users.noreply.github.com>
2023-11-11 06:22:33 +0000

    [Add]production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.aws_credentials = {
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ap-northeast-1
+    region: 'ap-northeast-1'
   }
 
   # Mount Action Cable outside main process or domain.
